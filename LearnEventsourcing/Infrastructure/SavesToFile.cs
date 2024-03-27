4c534d29266adead66c0c8e3b37de8872339ab14
Paul Austrem <paul.austrem@frende.no>
2024-03-27 08:43:09 +0100

    Tidy up

--- LearnEventsourcing/Infrastructure/SavesToFile.cs
+++ LearnEventsourcing/Infrastructure/SavesToFile.cs
@@ -12,19 +12,16 @@ namespace PresentationAndInfrastructure.Infrastructure
 
         public void Delete(int policyNumber)
         {
-            //delete InsurancePolicy from the static in-memory list called _data
             _data.RemoveAll(p => p.PolicyNumber == policyNumber);
         }
 
         public InsurancePolicy Read(int policyNumber)
         {
-            //return InsurancePolicy from the static in-memory list called _data, if no policy is found, return a new InsurancePolicy
             return _data.FirstOrDefault(p => p.PolicyNumber == policyNumber) ?? throw new DataPersistenceException("Policy not found");
         }
 
         public void Store(InsurancePolicy policy)
-        {
-            //save InsurancePolicy to the static in-memory list called _data. If an existing policy is found, update it, otherwise add it to the list
+        {            
             policy.PolicyNumber = _data.Count+1;
             _data.Add(policy);
         }
